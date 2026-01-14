96f050fe7c1d38fcfb9e8c0aa022693e6aff0f5f
Rethish.S <rethishsugu28@gmail.com>
2026-01-14 19:43:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ async function send() {
 
   chat.innerHTML += `<div class="user">You: ${text}</div>`;
   input.value = "";
-
-  const res = await fetch("http://127.0.0.1:5000/ask", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ question: text })
-  });
-
-  const data = await res.json();
-  chat.innerHTML += `<div class="bot">Rekash: ${data.answer}</div>`;
   chat.scrollTop = chat.scrollHeight;
+
+  try {
+    const res = await fetch("http://127.0.0.1:5000/ask", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ question: text })
+    });
+
+    const data = await res.json();
+    chat.innerHTML += `<div class="bot">Rekash: ${data.answer}</div>`;
+    chat.scrollTop = chat.scrollHeight;
+
+  } catch (error) {
+    chat.innerHTML += `<div class="bot">⚠️ Backend not running</div>`;
+  }
 }
